e3777a527d983c5a13406f991c3ca392a45d1c5e
Anton <huliovalente@gmail.com>
2018-09-23 19:21:48 +0300

    Fix
    new code

--- src/index.js
+++ src/index.js
@@ -1,31 +1,57 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.elements = [];
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.elements.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.elements[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.elements.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.elements;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        let sortedElement = indices
+            .map(index => this.elements[index])
+            .sort(this.comparator);
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+        if (this.isDecrease(indices)) {
+            sortedElement.reverse();
+        }
+
+        sortedElement.forEach(
+            (element, index) => (this.elements[indices[index]] = element)
+        );
+    }
+
+    isDecrease(indices) {
+        const firstIndex = indices[0];
+        const lastIndex = indices[indices.length - 1];
+
+        if (firstIndex > lastIndex) return true;
+        return false;
+    }
+
+    comparator(a, b) {
+        return a - b;
+    }
+
+    setComparator(compareFunction) {
+        this.comparator = compareFunction;
+    }
+
+    clearComparator() {
+        this.savedComparator = null;
+    }
 }
 
 module.exports = Sorter;
